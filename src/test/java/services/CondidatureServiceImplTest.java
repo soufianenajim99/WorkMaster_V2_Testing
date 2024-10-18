28ffdc17eff1b6e77dd3ed89daa004355529c1e7
soufianenajim99 <soufianenajim100@gmail.com>
2024-10-18 10:46:52 +0100

    WVT-9 test du service JobOffre

---
+++ src/test/java/services/CondidatureServiceImplTest.java
@@ -0,0 +1,44 @@
+package services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import entities.JobOffer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import repositories.repositoryinterfaces.JobOfferRepository;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CondidatureServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
